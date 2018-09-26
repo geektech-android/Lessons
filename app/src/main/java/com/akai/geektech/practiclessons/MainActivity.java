d836068939adcd337093b5f5cff3b787163ca9b0
Kudaybergen <tuleubekov.kas@gmail.com>
2018-09-26 12:58:31 +0600

    Add dynamic fragments

--- app/src/main/java/com/akai/geektech/practiclessons/MainActivity.java
+++ app/src/main/java/com/akai/geektech/practiclessons/MainActivity.java
@@ -1,13 +1,42 @@
 package com.akai.geektech.practiclessons;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
+    private FragmentFirst firstFragment;
+    private SecondFragment secondFragment;
+    private CheckBox checkStack;
+    private FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkStack = findViewById(R.id.checkBoxStack);
+        firstFragment = new FragmentFirst();
+        secondFragment = new SecondFragment();
+    }
+
+    public void onClick(View view) {
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        switch (view.getId()) {
+            case R.id.button_add:
+                fragmentTransaction.add(R.id.frame_container, firstFragment);
+                break;
+            case R.id.button_remove:
+                fragmentTransaction.remove(firstFragment);
+                break;
+            case R.id.button_replace:
+                fragmentTransaction.replace(R.id.frame_container, secondFragment);
+                break;
+        }
+        if (checkStack.isChecked()) {
+            fragmentTransaction.addToBackStack(null);
+        }
+        fragmentTransaction.commit();
     }
 }
