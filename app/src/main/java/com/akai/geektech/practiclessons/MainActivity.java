b12e17bb449151e0b810dcf7b622608ba7e04499
Kudaybergen <tuleubekov.kas@gmail.com>
2018-10-16 21:53:15 +0600

    Lesson2.4

--- app/src/main/java/com/akai/geektech/practiclessons/MainActivity.java
+++ app/src/main/java/com/akai/geektech/practiclessons/MainActivity.java
@@ -1,13 +1,90 @@
 package com.akai.geektech.practiclessons;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "TestMainActivity";
+    private TextView textView;
+    private Button button;
+    private int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.btn1);
+
+        textView = findViewById(R.id.textView);
+
+        if (savedInstanceState != null) {
+            count = savedInstanceState.getInt("count_key");
+        }
+
+        textView.setText(String.valueOf(count));
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+        Log.d(TAG, "onCreate");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState");
+        outState.putInt("count_key", count);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d(TAG, "onRestoreInstanceState");
+        count = savedInstanceState.getInt("count_key");
+        textView.setText(String.valueOf(count));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    public void onClick(View view) {
+        textView.setText(String.valueOf(++count));
     }
 }
