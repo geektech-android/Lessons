9d719779160c9e2ec43df956766584ee4ab391d8
RustamKeneev <dr.rustamkeneev@gmail.com>
2018-10-22 03:24:25 +0600

    HW4_lesson2.5_Rustam

--- app/src/main/java/com/akai/geektech/practiclessons/MainActivity.java
+++ app/src/main/java/com/akai/geektech/practiclessons/MainActivity.java
@@ -1,13 +1,30 @@
 package com.akai.geektech.practiclessons;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+private EditText editTextLogin, editTextPassword;
+private Button buttonSignIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextLogin = findViewById(R.id.editLogin);
+        editTextPassword = findViewById(R.id.editPassword);
+        buttonSignIn = findViewById(R.id.buttonSignIn);
+        buttonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (editTextLogin.getText().toString().equals("Rustam") && editTextPassword.getText().toString().equals("Keneev")){
+                    Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
