bea8531efc114fb178b25d1ceaa451b92c964d42
Kudaybergen <tuleubekov.kas@gmail.com>
2018-11-20 20:56:04 +0600

    Service

---
+++ app/src/main/java/com/akai/geektech/practiclessons/HardWorkService.java
@@ -0,0 +1,48 @@
+package com.akai.geektech.practiclessons;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import java.util.concurrent.TimeUnit;
+
+public class HardWorkService extends Service {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("TAGGER", "onCreate");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("TAGGER", "onStartCommand");
+        someTask();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("TAGGER", "onDestroy");
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private void someTask() {
+        for (int i = 0; i < 10; i++) {
+            try {
+                Log.d("TAGGER", "i: " + i);
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- app/src/main/java/com/akai/geektech/practiclessons/MainActivity.java
+++ app/src/main/java/com/akai/geektech/practiclessons/MainActivity.java
@@ -1,13 +1,55 @@
 package com.akai.geektech.practiclessons;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView mText;
+    private ProgressBar mProgress;
+    private Button mBtnStart, mBtnStop, mBtnCheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mText = findViewById(R.id.text_log);
+        mProgress = findViewById(R.id.progress);
+
+        mBtnStart = findViewById(R.id.btn_start);
+        mBtnStop = findViewById(R.id.btn_stop);
+        mBtnCheck = findViewById(R.id.btn_check_lock);
+
+        mBtnStart.setOnClickListener(this);
+        mBtnStop.setOnClickListener(this);
+        mBtnCheck.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_start: startHardWork();
+                break;
+            case R.id.btn_stop: stopHardWork();
+                break;
+            case R.id.btn_check_lock: checkUiThread();
+                break;
+        }
+    }
+
+    private void startHardWork() {
+        startService(new Intent(this, HardWorkService.class));
+    }
+
+    private void stopHardWork() {
+        stopService(new Intent(this, HardWorkService.class));
+    }
+
+    private void checkUiThread() {
+
     }
 }
